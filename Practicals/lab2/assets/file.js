4a4576969eb38726549338f5f54da69995331923
NathanHeaton <c00286899@setu.ie>
2024-02-01 12:43:02 +0000

    added form back end and fixed readme

--- Practicals/lab2/assets/file.js
+++ Practicals/lab2/assets/file.js
@@ -1,4 +1,65 @@
 window.onload = function(){
-    alert("welcome to my website")
+
+
 };
 
+let username = "name";
+let email = "email";
+let DOB = "10102005";
+let fan = 0;
+
+function validateForm()
+{
+    username = document.getElementById("username").value;
+    email = document.getElementById("useremail").value;
+    DOB = document.getElementById("DOB").value;
+    console.log("form is being validate")
+    if(username == "")
+    {
+        alert("please fill text feilds");
+    }
+    else
+    {
+        saveInfo();
+    }
+
+    
+
+
+}
+
+
+function saveInfo()
+{
+
+
+   // fan = document.getElementById("fan").value;
+    alert("form is being validate" + username +" is your name")
+
+    // sets all of the user data to local storage
+    localStorage.setItem("username", username);
+    localStorage.setItem("email", email);
+    localStorage.setItem("DOB", DOB);
+    localStorage.setItem("fan", fan);
+    
+
+}
+
+function retriveData()
+{
+    if(localStorage.getItem("name")!="")
+    {
+        username = localStorage.getItem("username");
+        email = localStorage.getItem("email");
+        DOB = localStorage.getItem("DOB");
+        fan = localStorage.getItem("fan");
+        console.log("username "+ username);
+        console.log("email: "+ email);
+        console.log("DOB "+ DOB);
+        console.log("fan "+ fan);
+    }
+    else
+    {
+        alert("no saved data");
+    }
+}
