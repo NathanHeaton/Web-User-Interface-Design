353c04c4e8f3191bcc1f32dce502f7327efd0ef9
Nathan Heaton <nathanmheaton@gmail.com>
2025-02-28 14:28:14 +0000

    replaced name to fix naming issue again

--- assets/file.js
+++ assets/file.js
@@ -39,7 +39,7 @@ let previousEnlargedWidth= null ;
 let openCards = [{name:"VLE", open: false, previousWidth: 66, classIndex: 1, tab: 6},
                 {name:"DN", open: false, previousWidth: 66, classIndex: 4, tab: 6},
                 {name:"STROW", open: false, previousWidth: 66, classIndex: 0, tab: 1},
-                {name:"PHIAST", open: false, previousWidth: 66, classIndex: 10, tab: 7},
+                {name:"PHIAST", open: false, previousWidth: 66, classIndex: 11, tab: 7},
                 {name:"RETROGRADE", open: false, previousWidth: 66, classIndex: 2, tab: 6},
                 {name:"CAR", open: false, previousWidth: 66, classIndex: 11, tab: 2}
             ]; // stores cards that can be opened from other pages
